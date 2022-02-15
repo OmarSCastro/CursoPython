338c8c5a330d66d9f97b98c5797821c3cd58a1b2
OmarSCastro <99294488+OmarSCastro@users.noreply.github.com>
2022-02-14 23:31:32 -0600

    Adding katas 8,9 y 10

---
+++ Kata1/Hola.py
@@ -0,0 +1,6 @@
+print("Bienvenido al programa de prueba")
+numero1= input("Introduzca el primer numero: ")
+numero2= input("Introduzca el segundo numero: ")
+print("El resultado es: ",int(numero1) + int(numero2))
+resultado=numero1+numero2
+print("El resultado es: ",resultado)
